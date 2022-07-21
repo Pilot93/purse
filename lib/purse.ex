a4c51b75b563155670abe976acb09a4eef58724f
Pilot93 <pilotfdd@gmail.com>
2022-07-21 19:00:53 +0600

    * lib/purse.ex: changed Server to GenServer

--- lib/purse.ex
+++ lib/purse.ex
@@ -1,84 +1,36 @@
 defmodule PurseServer do
-  @moduledoc """
-  Documentation for `Purse`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-  iex> Purse.hello()
-  :world
-
-  """
   def start do
-    ServerProcess.start(PurseServer)
+    GenServer.start(PurseServer, nil)
   end
 
   def add_entry(todo_server, new_entry) do
-    ServerProcess.cast(todo_server, {:add_entry, new_entry})
+    GenServer.cast(todo_server, {:add_entry, new_entry})
   end
 
   def entries(todo_server, date) do
-    ServerProcess.call(todo_server, {:entries, date})
+    GenServer.call(todo_server, {:entries, date})
   end
 
-  def init do
-    TodoList.new()
+  @impl GenServer
+  def init(_) do
+    {:ok, TodoList.new()}
   end
 
+  @impl GenServer
   def handle_cast({:add_entry, new_entry}, todo_list) do
-    TodoList.add_entry(todo_list, new_entry)
+    new_state = TodoList.add_entry(todo_list, new_entry)
+    {:noreply, new_state}
   end
 
-  def handle_call({:entries, date}, todo_list) do
-    {TodoList.entries(todo_list, date), todo_list}
-  end
-end
-
-defmodule ServerProcess do
-  def start(callback_module) do
-    spawn(fn ->
-      initial_state = callback_module.init
-      loop(callback_module, initial_state)
-    end)
-  end
-
-  defp loop(callback_module, current_state) do
-    receive do
-      {:call, request, caller} ->
-        {response, new_state} =
-          callback_module.handle_call(
-            request,
-            current_state
-          )
-
-        send(caller, {:response, response})
-        loop(callback_module, new_state)
-
-      {:cast, request} ->
-        new_state =
-          callback_module.handle_cast(
-            request,
-            current_state
-          )
-
-        loop(callback_module, new_state)
-    end
-  end
-
-  def call(server_pid, request) do
-    send(server_pid, {:call, request, self()})
-
-    receive do
-      {:response, response} ->
-        response
-    end
-  end
-
-  def cast(server_pid, request) do
-    send(server_pid, {:cast, request})
+  @impl GenServer
+  def handle_call({:entries, date}, _, todo_list) do
+    {
+      :reply,
+      TodoList.entries(todo_list, date),
+      todo_list
+    }
   end
 end
 

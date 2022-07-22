2ddcd82abb21e44318e22d730fbcb55152eb537d
Pilot93 <pilotfdd@gmail.com>
2022-07-22 16:07:47 +0600

    purse primitive logic without currencies

--- lib/purse.ex
+++ lib/purse.ex
@@ -49,44 +49,11 @@ defmodule Purse do
   end
 
   def deposit(purse, currency, amount) do
+    %Purse{purse | deposit: purse.deposit + currencies.currency * amount}
   end
 
-  def withdraw(purse, currency, amount) do
-  end
-end
-
-defmodule TodoList do
-
-  def add_entry(todo_list, entry) do
-    entry = Map.put(entry, :id, todo_list.auto_id)
-    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
-
-    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
-  end
-
-  def entries(todo_list, date) do
-    todo_list.entries
-    |> Stream.filter(fn {_, entry} -> entry.date == date end)
-    |> Enum.map(fn {_, entry} -> entry end)
-  end
-
-  def update_entry(todo_list, %{} = new_entry) do
-    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
-  end
-
-  def update_entry(todo_list, entry_id, updater_fun) do
-    case Map.fetch(todo_list.entries, entry_id) do
-      :error ->
-        todo_list
-
-      {:ok, old_entry} ->
-        new_entry = updater_fun.(old_entry)
-        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
-        %TodoList{todo_list | entries: new_entries}
-    end
-  end
-
-  def delete_entry(todo_list, entry_id) do
-    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
+  def withdraw(purse, currency, amount) when purse.deposit > amount do
+    %Purse{purse | deposit: purse.deposit - currencies.currency * amount}
   end
 end
+

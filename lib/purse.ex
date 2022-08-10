8077af876a5a768ea6d71cbe39381b256a8d6656
Pilot93 <pilotfdd@gmail.com>
2022-08-10 12:10:01 +0600

    purse with ETS based on key/value

--- lib/purse.ex
+++ lib/purse.ex
@@ -1,59 +1,24 @@
-defmodule PurseServer do
+defmodule Purse do
   use GenServer
 
-  def start do
-    GenServer.start(PurseServer, nil)
+  def start_link do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  def add_entry(todo_server, new_entry) do
-    GenServer.cast(todo_server, {:add_entry, new_entry})
+  def put(key, value) do
+    :ets.insert(__MODULE__, {key, value})
   end
 
-  def entries(todo_server, date) do
-    GenServer.call(todo_server, {:entries, date})
+  def get(key) do
+    case :ets.lookup(__MODULE__, key) do
+      [{^key, value}] -> value
+      [] -> nil
+    end
   end
 
   @impl GenServer
   def init(_) do
-    {:ok, TodoList.new()}
-  end
-
-  @impl GenServer
-  def handle_cast({:add_entry, new_entry}, todo_list) do
-    new_state = TodoList.add_entry(todo_list, new_entry)
-    {:noreply, new_state}
-  end
-
-  @impl GenServer
-  def handle_call({:entries, date}, _, todo_list) do
-    {
-      :reply,
-      TodoList.entries(todo_list, date),
-      todo_list
-    }
+    :ets.new(__MODULE__, [:named_table, :protected, write_concurrency: true])
+    {:ok, nil}
   end
 end
-
-defmodule Purse do
-  @moduledoc """Implement purse that can hold money and can deposit or withdraw
-  in different currencies"""
-  defstruct auto_id: 1, accounts: %{}
-
-  def new(accounts \\ []) do
-    Enum.reduce(
-      accounts,
-      %Purse{},
-      &add_account(&2, &1)
-    )
-    IO.puts("You can deposit or withdraw in currencies: HIV, PIV, SIC")
-  end
-
-  def deposit(purse, currency, amount) do
-    %Purse{purse | deposit: purse.deposit + currencies.currency * amount}
-  end
-
-  def withdraw(purse, currency, amount) when purse.deposit > amount do
-    %Purse{purse | deposit: purse.deposit - currencies.currency * amount}
-  end
-end
-
